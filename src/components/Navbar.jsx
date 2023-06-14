64bd6534e7a76a72a26507f74a3c8f5ee97b1d1b
CodeSevenic <sevenicbookings@gmail.com>
2023-06-14 13:27:48 +0200

    Hub-Nabled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { FiShoppingCart } from 'react-icons/fi';
+import { BsChatLeft } from 'react-icons/bs';
+import { RiNotification3Line } from 'react-icons/ri';
 
 const Navbar = () => {
   return <div>Navbar</div>;
