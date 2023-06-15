d123a4fb84363b19c6459ce9a16d5d7729ef4e57
CodeSevenic <sevenicbookings@gmail.com>
2023-06-15 14:21:16 +0200

    Hub-Nabled

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,20 @@
 ﻿import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine = () => {
-  return <div>SparkLine</div>;
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      type={type}
+      fill={color}
+      dataSource={data}
+      border={{ color: currentColor, width: 2 }}
+    ></SparklineComponent>
+  );
 };
 
 export default SparkLine;
