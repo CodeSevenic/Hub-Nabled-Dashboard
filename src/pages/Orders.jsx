0bbfe100c3fc15b054361187c48dd826fa12d1dc
CodeSevenic <sevenicbookings@gmail.com>
2023-06-17 14:32:14 +0200

    Hub-Nabled

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -21,7 +21,7 @@ const Orders = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
-      <GridComponent id="gridcomp" dataSource={ordersData} allowPaging>
+      <GridComponent id="gridcomp" dataSource={ordersData} allowPaging={true} allowSorting={true}>
         <ColumnsDirective>
           {ordersGrid.map((column, index) => (
             <ColumnDirective key={index} {...column} />
