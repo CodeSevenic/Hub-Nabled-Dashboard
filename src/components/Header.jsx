c97c222eabf97b2d64be848a5f8adb7eede94565
CodeSevenic <sevenicbookings@gmail.com>
2023-06-17 14:05:12 +0200

    Hub-Nabled

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 ﻿import React from 'react';
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ category, title }) => {
+  return (
+    <div className="mb-10">
+      <p className="text-gray-400">{category}</p>
+    </div>
+  );
 };
 
 export default Header;
